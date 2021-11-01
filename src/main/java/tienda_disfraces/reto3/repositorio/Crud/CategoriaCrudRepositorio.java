ecb9b45f87c2fd5498115a3428ae1743613faa37
Your Name <belkiz.galvis.mt@correo.usa.edu.co>
2021-11-01 18:19:23 -0500

    comentario

---
+++ src/main/java/tienda_disfraces/reto3/repositorio/Crud/CategoriaCrudRepositorio.java
@@ -0,0 +1,12 @@
+package tienda_disfraces.reto3.repositorio.Crud;
+
+import tienda_disfraces.reto3.modelo.Categoria;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @autor Belkiz Galvis
+ */
+public interface CategoriaCrudRepositorio extends CrudRepository <Categoria,Integer> {
+    
+}
